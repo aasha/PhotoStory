ccd7671145f322dc64b207d264063977db58ec56
Guduri Sriram <shreeram.guduri@gmail.com>
2016-05-27 11:24:42 +0530

    Added classes for GetPersonDetailsRequest and GetPersonDetailsResponse in retrofit

---
+++ AndroidClient/app/src/main/java/com/pixtory/app/retrofit/GetPersonDetailsRequest.java
@@ -0,0 +1,8 @@
+package com.pixtory.app.retrofit;
+
+/**
+ * Created by krish on 26/05/2016.
+ */
+public class GetPersonDetailsRequest {
+    public int userId;
+}
---
+++ AndroidClient/app/src/main/java/com/pixtory/app/retrofit/GetPersonDetailsResponse.java
@@ -0,0 +1,17 @@
+package com.pixtory.app.retrofit;
+
+import com.pixtory.app.model.ContentData;
+import com.pixtory.app.model.PersonInfo;
+
+import java.util.ArrayList;
+
+/**
+ * Created by krish on 26/05/2016.
+ */
+public class GetPersonDetailsResponse {
+    public boolean success;
+    public int errorCode;
+    public String errorMessage;
+    public PersonInfo personDetails;
+    public ArrayList<ContentData> contentList;
+}
